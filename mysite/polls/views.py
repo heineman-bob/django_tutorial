553e2a4ab72204f112c39f406fcee67abe9eeccb
Bob Heineman <bheineman@rushst.com>
2022-10-04 10:52:51 -0500

    initial app functionality

---
+++ mysite/polls/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+
+from django.shortcuts import render
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except:
+        raise Http404("Question does not exist")
+    
+    return render(request, 'polls/detail.html', {'question':question})
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
+
